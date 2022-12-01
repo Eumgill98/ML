04c5aa9c7d1b7625849d3272b6e55bcae2a4b8b7
wjdghcks6735 <wjdghcks02@gmail.com>
2022-12-01 13:15:04 +0900

    단순 선형회귀 구현 1.

---
+++ LinearRegression/LinearRegression(simple).py
@@ -0,0 +1,44 @@
+import numpy as np
+
+from sklearn.datasets import load_diabetes
+database = load_diabetes()
+
+#예시 데이터
+x = database.data[:,2]
+y = database.target
+
+#초기 weight값 설정
+def initialization_wb():
+    weight = 0.0
+    bias = 0.0
+    return weight, bias
+
+#예측 y값 계산 함수 정의
+def calculate(weight, x, bias):
+    y_hat = weight * x + bias
+    return y_hat
+
+#비용 함수 정의
+def cost_calculate(y, y_hat):
+    cost = ((y - y_hat) ** 2).mean()
+
+
+def fit(x, y, epoch=10000, lr = 0.5):
+    epoch = epoch
+    lr = lr
+    weight, bias = initialization_wb()
+
+    for i in range(epoch):
+        y_hat = calculate(weight, x, bias)
+        cost = cost_calculate(y, y_hat)
+
+        #가중치 및 변향 업데이트
+        weight -= lr * ((y_hat - y) * x).mean()
+        bias -= lr * (y_hat - y).mean()
+
+        if(i + 1) % 1000 == 0:
+            print(f"Epoch : {i}, Weight : {weight}, Bias : {bias}, Cost : {cost}")
+
+#run!
+fit(x, y, epoch=10000, lr=0.5)
+
